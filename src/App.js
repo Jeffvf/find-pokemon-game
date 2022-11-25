5ee1b26f8f04fe23a7ffbe7ea860ddcfe04af490
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-11-25 11:45:29 -0300

    feat: implement firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Scoreboard from "./pages/Scoreboard";
 import PlayGame from "./pages/PlayGame";
+import { initializeApp } from "firebase/app";
 
 function App() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyB6x84ueab6E9VGnjNJS35Sf_sO-_Iv2EY",
+    authDomain: "find-character-game.firebaseapp.com",
+    projectId: "find-character-game",
+    storageBucket: "find-character-game.appspot.com",
+    messagingSenderId: "720025025940",
+    appId: "1:720025025940:web:aa302976b45ec2bd50a381"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PlayGame />}></Route>
+        <Route path="/" element={<PlayGame app={app}/>}></Route>
         <Route path="/scoreboard" element={<Scoreboard />}></Route>
       </Routes>
     </BrowserRouter>
